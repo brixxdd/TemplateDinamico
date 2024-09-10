c442fab49c53f59ef3912c421660570947a2ec65
brixxd <brian.flores99@unach.mx>
2024-09-10 17:27:47 -0600

    Guardar cambios locales en nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,8 +7,20 @@ export default defineNuxtConfig({
   ],
   app: {
     head: {
+      link: [
+        // Añadir FontAwesome usando la etiqueta <link>
+        {
+          rel: 'stylesheet',
+          href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css',
+        },
+      ],
       script: [
-        { src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js', type: 'text/javascript', defer: true }
+        // Añadir Bootstrap script
+        {
+          src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js',
+          type: 'text/javascript',
+          defer: true,
+        },
       ]
     }
   },
