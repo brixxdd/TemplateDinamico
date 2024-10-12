9c73b5034ec614806d9c7f5392aebc326f6b0f3d
Nahum <hector.sanchez68@unach.mx>
2024-10-12 10:56:05 -0600

    Primer commit con el código del proyecto

---
+++ components/Header.vue
@@ -0,0 +1,180 @@
+<template>
+  <header :class="{ transparent: !isScrolled, scrolled: isScrolled }" :style="headerStyle">
+    <nav class="navbar navbar-expand-lg navbar-light">
+      <a class="navbar-brand" href="#">CURSOS EN LINEA </a>
+      <!-- Botón de menú (hamburguesa) visible solo en pantallas pequeñas -->
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <!-- Elementos del menú -->
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto"> <!-- Menú alineado a la derecha en pantallas grandes -->
+          <li class="nav-item">
+            <a class="nav-link" href="#inicio"><span>Inicio</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#quienes-somos"><span>Nosotros</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#servicios"><span>Servicios</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#contacto"><span>Contacto</span></a>
+          </li>          
+        </ul>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+import colors from '@/public/data/colors.json';
+
+export default {
+  props: {
+    isScrolled: Boolean
+  },
+  computed: {
+    headerStyle() {
+      return {
+        backgroundColor: this.isScrolled ? colors.header.scrolledBackgroundColor : colors.header.backgroundColor,
+      };
+    }
+  }
+};
+</script>
+
+<style scoped>
+/* Estilos del header */
+header {
+  position: fixed;
+  top: 0;
+  width: 100%;
+  z-index: 1000;
+  color: #ffffff;
+  transition: background-color 0.3s;
+}
+
+/* Clase dinámica cuando se ha hecho scroll */
+header.transparent {
+  background-color: transparent;
+}
+
+header.scrolled {
+  background-color: #0D47A1; /* Un azul oscuro más intenso */
+}
+
+/* Estilos del navbar */
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 1200px;
+  margin: 0 auto;
+  width: 100%;
+}
+
+.navbar-brand {
+  font-size: 24px;
+  font-weight: bold;
+  color: #d21436; /* Amarillo brillante */
+  margin-right: auto;
+}
+
+.navbar-nav {
+  display: flex;
+  flex-direction: row;
+  margin-left: auto;
+}
+
+.nav-item {
+  margin-left: 20px;
+}
+
+.nav-link {
+  background-image: linear-gradient(150deg, #FF6F61, #FFB74D, #64B5F6); /* Gradiente llamativo */
+  border-radius: 8px;
+  color: #fff;
+  display: inline-flex;
+  align-items: center;
+  font-size: 16px;
+  padding: 4px;
+  cursor: pointer;
+  transition: 0.3s;
+  text-decoration: none;
+}
+
+.nav-link span {
+  background-color: #263238; /* Fondo gris oscuro */
+  padding: 8px 16px;
+  border-radius: 6px;
+  transition: 0.3s;
+}
+
+.nav-link:hover span {
+  background: none;
+}
+
+/* Botón "Regístrate" */
+.btn-register {
+  background-image: linear-gradient(150deg, #FF7043, #26ff9a, #42A5F5); /* Otro gradiente vibrante */
+  border-radius: 8px;
+  color: #fff;
+  padding: 8px 16px;
+  border: none;
+  cursor: pointer;
+  transition: background 0.3s;
+}
+
+.btn-register span {
+  background-color: #263238; /* Fondo gris oscuro */
+  padding: 8px 16px;
+  border-radius: 6px;
+}
+
+.btn-register:hover span {
+  background: none;
+}
+
+/* Ajustes para pantallas pequeñas */
+@media (max-width: 992px) {
+  .navbar-collapse {
+    background: rgba(0, 0, 0, 0.9); /* Fondo oscuro sólido */
+    padding: 10px;
+    border-radius: 8px;
+    position: relative;
+    z-index: 999;
+    max-height: 80vh;
+    border-bottom: 2px solid rgba(255, 255, 255, 0.2);
+    overflow-y: auto;
+  }
+
+  .navbar-collapse.show {
+    position: fixed;
+    top: 80px;
+    left: 0;
+    right: 0;
+    background: rgba(0, 0, 0, 0.9); /* Fondo sólido cuando el menú está abierto */
+    border-bottom: 2px solid rgba(255, 255, 255, 0.2);
+  }
+
+  .navbar-nav {
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .nav-item {
+    margin-left: 0;
+    margin-top: 10px;
+  }
+
+  .btn-register {
+    margin-top: 20px;
+    width: 100%;
+  }
+}
+
+body {
+  padding-top: 80px;
+}
+</style>
---
+++ components/HeroSlideshow.vue
@@ -0,0 +1,126 @@
+<template>
+  <swiper
+    :effect="'cube'"
+    :cubeEffect="{ slideShadows: true, shadowOffset: 20, shadowScale: 0.94 }"
+    :slidesPerView="1"
+    :spaceBetween="30"
+    :loop="true"
+    :autoplay="{
+      delay: 5000,
+      disableOnInteraction: false,
+    }"
+    :modules="modules"
+    class="mySwiper"
+  >
+    <swiper-slide v-for="(slide, index) in slides" :key="index">
+      <div class="slide-content">
+        <!-- Cambié el video por imágenes -->
+        <img class="slide-image" :src="slide.imageSrc" alt="Imagen del slide" />
+        <div class="slide-caption">
+          <h2>{{ slide.title }}</h2>
+          <p>{{ slide.description }}</p>
+        </div>
+      </div>
+    </swiper-slide>
+  </swiper>
+</template>
+
+<script>
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import 'swiper/css';
+import 'swiper/css/effect-cube';
+import { EffectCube, Autoplay } from 'swiper/modules';
+
+export default {
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  setup() {
+    return {
+      modules: [EffectCube, Autoplay],
+      slides: [
+  {
+    imageSrc: '/imagen1.jpg',
+    title: "Descubre el Aprendizaje en Línea",
+    description: "¡Explora un mundo de aprendizaje en línea! Adquiere nuevas habilidades desde la comodidad de tu hogar.",
+  },
+  {
+    imageSrc: '/imagen2.jpg',
+    title: "Conocimiento al Alcance de un Clic",
+    description: "Con nuestros cursos en línea, el conocimiento está al alcance de un clic. ¡Empieza tu aventura educativa hoy!",
+  },
+  {
+    imageSrc: '/imagen3.jpg',
+    title:  "Aprende a tu Ritmo",
+    description: "Aprende a tu ritmo y conviértete en un experto en temas que te apasionan, ¡todo a través de nuestra plataforma en línea!",
+  },
+  {
+    imageSrc: '/imagen4.jpg',
+    title:  "Aprendizaje Innovador",
+    description: "¡Conéctate con un aprendizaje innovador! Desarrolla tus habilidades y prepárate para el futuro desde casa.",
+  },
+]
+
+    };
+  },
+};
+</script>
+
+<style scoped>
+#app {
+  height: 100%;
+}
+html,
+body {
+  position: relative;
+  height: 100%;
+}
+
+body {
+  background: hsl(180, 66%, 49%);
+  font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
+  font-size: 14px;
+  color: #000;
+  margin: 0;
+  padding: 0;
+}
+
+.swiper {
+  width: 100%;  /* Ocupa todo el ancho del contenedor */
+  height: 100%; /* Ocupa todo el alto del contenedor */
+}
+
+.swiper-slide {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #fff;
+}
+
+.slide-content {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+
+.slide-image {
+  width: 90%;     /* Ocupa todo el ancho del contenedor */
+  height: 90%;    /* Ocupa todo el alto del contenedor */
+  object-fit: cover; /* Escala y recorta la imagen si es necesario, para llenar el contenedor */
+  z-index: 1;
+}
+
+.slide-caption {
+  position: absolute;
+  bottom: 20px;
+  left: 0;
+  right: 0;
+  text-align: center;
+  color: white;
+  background: rgba(0, 0, 0, 0.5);
+  padding: 10px;
+  z-index: 2;
+}
+
+</style>
